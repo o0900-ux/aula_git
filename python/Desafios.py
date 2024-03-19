d3621aafbf0fcc008edbaaaed6c1d0306cd4c1f9
o0900-ux <kmdskmds2@gmail.com>
2024-03-19 17:20:59 -0300

    arquivo de aula

---
+++ python/Desafios.py
@@ -0,0 +1,51 @@
+print('--------desafio1-------')
+p=input('digite: ')
+
+v='aeiouAEIOU'
+n_v=0
+n_c=0
+
+for l in p:
+    if l.isalpha():
+        if l in v:
+            n_v += 1
+        else:
+            n_c +=1
+
+print("Número de vogais: ",n_v, "\nNúmero de consoantes: ", n_c, '\n--------fim-------')
+
+print('--------desafio2-------')
+dias_uteis = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta']
+dias_digitados = []
+
+for i in range(7):
+    dia = input("Digite o dia da semana: ").capitalize()
+    if dia in dias_uteis:
+        dias_digitados.append(dia + "-feira")
+
+print("\nDias úteis da semana digitados pelo usuário:")
+for dia in dias_digitados:
+    print(dia)
+print('--------fim-------')
+
+print('--------desafio3-------')
+numero = int(input("Digite um numero  para verificar se e primo: "))
+
+if numero <= 1:
+    print(numero, "não e um numero primo.")
+else:
+    primo = True
+    for i in range(2, int(numero ** 0.5) + 1):
+        if numero % i == 0:
+            primo = False
+            break
+    if primo:
+        print(numero, "e um numero primo.")
+    else:
+        print(numero, "não é um número primo.")
+print('--------fim-------')
+
+
+
+
+
