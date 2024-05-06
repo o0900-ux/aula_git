0bf6a989c54687230f84f0df5bed43ee111a91ef
o0900-ux <139592950+o0900-ux@users.noreply.github.com>
2024-05-06 10:17:42 -0300

    kivy login tela 2

--- python/Arquivos_AulaAtividade/login.py
+++ python/Arquivos_AulaAtividade/login.py
@@ -3,13 +3,17 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.image import AsyncImage
 from kivy.core.window import Window
+from kivy.utils import get_color_from_hex
 
 class Telalogin(BoxLayout):
     def __init__(self, **kwargs):
         super(Telalogin, self).__init__(**kwargs)
         self.orientation = "vertical"
         self.padding = 20
+
+        self.add_widget(AsyncImage(source='https://github.com/MateusN17/Reposit-rio-/blob/main/Kivy/images.png?raw=true'))
         
         self.add_widget(Label(text="Insira o seu E-mail"))
         self.Email = TextInput(hint_text="Digite o Seu E-mail")
@@ -19,14 +23,14 @@ class Telalogin(BoxLayout):
         self.Senha = TextInput(hint_text="Digite sua senha", password=True)
         self.add_widget(self.Senha)
         
-        botao_layout = BoxLayout(padding=10)
+        botao_layout = BoxLayout(padding=8)
         self.add_widget(botao_layout)
         
-        self.enviar = Button(text="Enviar")
+        self.enviar = Button(text="Enviar", background_color=get_color_from_hex('#3498db'))
         self.enviar.bind(on_press=self.Login)
         botao_layout.add_widget(self.enviar)
         
-        self.cancelar = Button(text="Cancelar")
+        self.cancelar = Button(text="Cancelar", background_color=get_color_from_hex('#ff0000'))
         self.cancelar.bind(on_press=self.botao_cancelar)
         botao_layout.add_widget(self.cancelar)
         
